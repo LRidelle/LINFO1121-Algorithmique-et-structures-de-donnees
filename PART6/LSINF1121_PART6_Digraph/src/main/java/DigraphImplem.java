2f83632be00b2f1de1e7160347016e9a638b02ba
ridellel <louis.ridelle@student.uclouvain.be>
2021-12-13 13:57:25 +0100

    DigraphImpl Part6

--- PART6/LSINF1121_PART6_Digraph/src/main/java/DigraphImplem.java
+++ PART6/LSINF1121_PART6_Digraph/src/main/java/DigraphImplem.java
@@ -1,31 +1,39 @@
+import java.util.*;
+
 public class DigraphImplem implements Digraph {
 
+    public List<Integer>[] adjL;
+    public int E;
+    public int V;
 
     public DigraphImplem(int V) {
-         // TODO
+        adjL = (ArrayList<Integer>[]) new ArrayList[V];
+        for(int i=0; i < V; i++)
+            adjL[i] = new ArrayList<>();
+        this.V = V;
+        this.E = 0;
     }
 
     /**
      * The number of vertices
      */
     public int V() {
-        // TODO
-        return 0;
+        return V;
     }
 
     /**
      * The number of edges
      */
     public int E() {
-        // TODO
-        return 0;
+        return E;
     }
 
     /**
      * Add the edge v->w
      */
     public void addEdge(int v, int w) {
-        // TODO
+        adjL[v].add(w);
+        E++;
     }
 
     /**
@@ -33,17 +41,19 @@ public class DigraphImplem implements Digraph {
      * that is the nodes w such that there is an edge v->w
      */
     public Iterable<Integer> adj(int v) {
-        return null;
-        // TODO
+        return adjL[v];
     }
 
     /**
      * A copy of the digraph with all edges reversed
      */
     public Digraph reverse() {
-        return null;
-        // TODO
+        Digraph rev = new DigraphImplem(this.V());
+        for(int i=0; i<V(); i++){
+            for(Integer x : adj(i)){
+                rev.addEdge(x, i);
+            }
+        }
+        return rev;
     }
-
-
 }
