cf00071936edce64b53e2acff06d38cf1a1b019e
ridellel <louis.ridelle@student.uclouvain.be>
2021-12-28 09:01:09 +0100

    fix files

---
+++ PART6/LSINF1121_Part6_MineClimb/src/main/java/MineClimbing.java
@@ -0,0 +1,21 @@
+//feel free to import anything here
+
+public class MineClimbing {
+    /**
+    * Returns the minimum distance between (startX, startY) and (endX, endY), knowing that
+    * you can climb from one mine block (a,b) to the other (c,d) with a cost Math.abs(map[a][b] - map[c][d])
+    *
+    * Do not forget that the world is round: the position (map.length,i) is the same as (0, i), etc.
+    *
+    * map is a matrix of size n times m, with n,m > 0.
+    *
+    * 0 <= startX, endX < n
+    * 0 <= startY, endY < m
+    */
+    public static int best_distance(int[][] map, int startX, int startY, int endX, int endY) {
+        //TODO Student
+        return 0;
+    }
+
+    // you may need to add additional things below.
+}
