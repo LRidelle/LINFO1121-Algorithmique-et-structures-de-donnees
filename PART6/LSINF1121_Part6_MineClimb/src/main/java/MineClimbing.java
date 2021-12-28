218b797ca3cd7d54d1039100ab7119fb31da10a0
ridellel <louis.ridelle@student.uclouvain.be>
2021-12-28 11:59:50 +0100

    MineClimbing 21%

--- PART6/LSINF1121_Part6_MineClimb/src/main/java/MineClimbing.java
+++ PART6/LSINF1121_Part6_MineClimb/src/main/java/MineClimbing.java
@@ -1,5 +1,12 @@
 //feel free to import anything here
 
+import org.junit.Assert;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class MineClimbing {
     /**
     * Returns the minimum distance between (startX, startY) and (endX, endY), knowing that
@@ -14,8 +21,68 @@ public class MineClimbing {
     */
     public static int best_distance(int[][] map, int startX, int startY, int endX, int endY) {
         //TODO Student
-        return 0;
+        int[][] distTo = new int[map.length][map[0].length];
+        for(int[] l : distTo)
+            Arrays.fill(l, Integer.MAX_VALUE);
+
+        PriorityQueue<posAndDist> q = new PriorityQueue<>();
+        q.add(new posAndDist(startX, startY, 0));
+        distTo[startX][startY] = 0;
+
+        while(!q.isEmpty()){
+            posAndDist curr = q.poll();
+            if(curr.x == endX && curr.y==endY)
+                return distTo[endX][endY];
+
+            for(posAndDist neigh : adj(map, curr)){
+                if(distTo[neigh.x][neigh.y] > distTo[curr.x][curr.y] + neigh.dist){
+                    distTo[neigh.x][neigh.y] = distTo[curr.x][curr.y] + neigh.dist;
+                    q.add(neigh);
+                }
+            }
+        }
+        return distTo[endX][endY];
     }
 
     // you may need to add additional things below.
+    static class posAndDist implements Comparable<posAndDist> {
+        public int x;
+        public int y;
+        public int dist;
+
+        public posAndDist(int x, int y, int dist){
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(posAndDist o) { return this.dist - o.dist; }
+    }
+
+    static Iterable<posAndDist> adj(int[][] map, posAndDist curr){//int Xindex, int Yindex){
+        List<posAndDist> adj=new LinkedList<>();
+        //Add above
+        if(curr.x==0)
+            adj.add(new posAndDist(map.length-1, curr.y, Math.abs(map[curr.x][curr.y] - map[map.length-1][curr.y])));
+        else
+            adj.add(new posAndDist(curr.x-1, curr.y, Math.abs(map[curr.x][curr.y] - map[curr.x-1][curr.y])));
+        //Add below
+        if(curr.x==map.length-1)
+            adj.add(new posAndDist(0, curr.y, Math.abs(map[curr.x][curr.y] - map[0][curr.y])));
+        else
+            adj.add(new posAndDist(curr.x+1, curr.y, Math.abs(map[curr.x][curr.y] - map[curr.x+1][curr.y])));
+        //Add left
+        if(curr.y==0)
+            adj.add(new posAndDist(curr.x, map[0].length-1, Math.abs(map[curr.x][curr.y] - map[curr.x][map[0].length-1])));
+        else
+            adj.add(new posAndDist(curr.x, curr.y-1, Math.abs(map[curr.x][curr.y] - map[curr.x][curr.y-1])));
+        //Add right
+        if(curr.y==map[curr.x].length-1)
+            adj.add(new posAndDist(curr.x, 0, Math.abs(map[curr.x][curr.y] - map[curr.x][0])));
+        else
+            adj.add(new posAndDist(curr.x, curr.y+1, Math.abs(map[curr.x][curr.y] - map[curr.x][curr.y+1])));
+
+        return adj;
+    }
 }
