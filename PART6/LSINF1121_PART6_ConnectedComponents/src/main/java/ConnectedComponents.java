3652735e562b2a0a0018033fa35593522a983212
ridellel <louis.ridelle@student.uclouvain.be>
2021-12-30 11:07:09 +0100

    Connected compo

--- PART6/LSINF1121_PART6_ConnectedComponents/src/main/java/ConnectedComponents.java
+++ PART6/LSINF1121_PART6_ConnectedComponents/src/main/java/ConnectedComponents.java
@@ -1,26 +1,26 @@
 public class ConnectedComponents {
 
     private static boolean[] marked;
-    public static int comp = 0;
+    public static int comp;
 
     /**
      * @return the number of connected components in g
      */
     public static int numberOfConnectedComponents(Graph g) {
         marked = new boolean[g.V()];
+        comp = 0;
 
-        for(int i = 0; i <= g.V() && !marked[i]; i++){
-            dfs(g, i);
-            comp++;
+        for(int i = 0; i < g.V(); i++) {
+            if(!marked[i]){
+                dfs(g, i);
+                comp++;
+            }
         }
 
         return comp;
     }
 
     public static void dfs(Graph g, int v){
-        if(marked[v])
-            return;
-
         marked[v] = true;
         for(int i : g.adj(v)){
             if(!marked[i])
